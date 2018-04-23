e1ce42ada48e93d51e0cbd871be44efab858cb3c
Juniormartins26 <38302838+Juniormartins26@users.noreply.github.com>
2018-04-22 22:39:40 -0300

    Atualização calculando a media e exibindo o valor

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 (function(){
-	
 
+		  let nota1, nota2, media = 0;
+		  nota1 = document.querySelector('#nota01');
+		  nota2 = document.querySelector('#nota02');
+		  nota3 = document.querySelector('#nota03');
+
+		 let botao = document.querySelector('#btnCalcular');
+
+		 botao.addEventListener('click',mediaCal,false);
+			
+			function mediaCal(){	
+
+				nota1 = parseFloat(nota1.value);
+				nota2 = parseFloat(nota2.value);
+				nota3 = parseFloat(nota3.value);
+
+				media = (nota1 + nota2 + nota3 )/3;
+				if(media){alert("A sua média é "+media.toFixed(2));
+				}
+
+
+				}
+					
+
+					
+
+				
+	
 }());
