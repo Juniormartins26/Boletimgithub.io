4eac6ac8e016c8325d0393e0fae218520630fc41
Juniormartins26 <38302838+Juniormartins26@users.noreply.github.com>
2018-04-30 21:26:40 -0300

    Valicação

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 		 let botao = document.querySelector('#btnCalcular');
 
 		 botao.addEventListener('click',mediaCal,false);
-			
+			limp();
 			function mediaCal(){	
 				if(nota1.value == "" || nota2.value == "" ||  nota3.value == ""){
 					alert('Preencha todos os campos!');
@@ -19,20 +19,32 @@
 				nota3 = parseFloat(nota3.value);
 
 				media = (nota1 + nota2 + nota3 )/3;
-				alert("A sua média é "+media.toFixed(2));
+				
 				botao.removeEventListener('click',mediaCal, true);
 					
-				
-				}
-			}
+				valicao();
 
-				
+				}
+			}		
+			function limp(){
 				btnLimpar.addEventListener('click', function(){
-					
+			
 					window.location.reload();
 					},false);
-			
-			
+			}
+				
+			function valicao(){
+				if(media >= 7){
+					alert("Aluno Aprovado com a média "+media.toFixed(2));
+
+				}
+				else if(media >=5  && media <7){
+					alert("Aluno Recuperação com a média "+media.toFixed(2));
+
+				}else{
+						alert("Aluno Reprovado com a média "+media.toFixed(2));
+				}
+			}
 			
 			
 				 
