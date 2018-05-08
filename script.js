17897a44651b9ef5d79590b9a2e1b15901493405
Juniormartins26 <38302838+Juniormartins26@users.noreply.github.com>
2018-05-07 21:25:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
-(function(){
+	(function(){
 
-		let nota1, nota2, media = 0;
-		  nota1 = document.querySelector('#nota01');
-		  nota2 = document.querySelector('#nota02');
-		  nota3 = document.querySelector('#nota03');
-		  let msg = document.querySelector('#msg-modal');
+			let nota1, nota2, media = 0;
+			  nota1 = document.querySelector('#nota01');
+			  nota2 = document.querySelector('#nota02');
+			  nota3 = document.querySelector('#nota03');
+			  let msg = document.querySelector('#msg-modal');
+			  let   msgObrigatorio = document.querySelector('#msg');
+			  let btnLimpar = document.querySelector('#btnLimpar');
+			 let botao =  document.querySelector('#btnCalcular');
 
-		  let btnLimpar = document.querySelector('#btnLimpar');
-		 let botao =  document.querySelector('#btnCalcular');
+			 botao.addEventListener('click',mediaCal,false);
+			 	// $('#btnCalcular').click (function(){
+			 	// 	$('#modal-mensagem').modal();
 
-		 botao.addEventListener('click',mediaCal,false);
-		 	// $('#btnCalcular').click (function(){
-		 	// 	$('#modal-mensagem').modal();
+			 	// });
 
-		 	// });
-
-			limp();
-			
-			function mediaCal(){	
-				if(nota1.value == "" || nota2.value == "" ||  nota3.value == ""){
-					alert('Preencha todos os campos!');
-
-			}else{
-				nota1 = parseFloat(nota1.value);
-				nota2 = parseFloat(nota2.value);
-				nota3 = parseFloat(nota3.value);
-
-				media = (nota1 + nota2 + nota3 )/3;
+				limp();
 				
-				botao.removeEventListener('click',mediaCal, true);
-					
-				valicao();
+				function mediaCal(){	
+					if(nota1.value == "" || nota2.value == "" ||  nota3.value == ""){
 
-				}
-			}		
-			function limp(){
-				btnLimpar.addEventListener('click', function(){
-			
-					 window.location.reload();
-		 	},false);
+						 $('.bd-show2-modal-sm ,modal-content').modal();
 
-			 
-					
-			}
-			 
-			
-			function valicao(){
-				if(media >= 7){
-				
-					 $('.bd-example-modal-sm ,#modal-content').modal();
-					
-					msg.innerText=" Aluno Aprovado com a média "+media.toFixed(2);
-
-				}
-				else if(media >=5  && media <7){
-					  $('.bd-example-modal-sm ,#modal-content').modal();
-					msg.innerText=(" O aluno ficou em  Recuperação com a média "+media.toFixed(2));
-
+						  msgObrigatorio.innerText=" Preencha todos os campos!";
 				}else{
-					 $('.bd-example-modal-sm ,#modal-content').modal();
-					msg.innerText=(" O aluno foi reprovado com a média "+media.toFixed(2));
+					nota1 = parseFloat(nota1.value);
+					nota2 = parseFloat(nota2.value);
+					nota3 = parseFloat(nota3.value);
 
+					media = (nota1 + nota2 + nota3 )/3;
+					
+					botao.removeEventListener('click',mediaCal, true);
+						
+					valicao();
+
+					}
+				}		
+				function limp(){
+					btnLimpar.addEventListener('click', function(){
+				
+						 window.location.reload();
+			 	},false);
+
+				 
+						
 				}
-			}
-			
-			
-		
+				 
+				
+				function valicao(){
+					if(media >= 7){
+					
+						 $('.bd-show-modal-sm ,modal-content').modal();
+						
+						msg.innerText=" Aluno Aprovado com a média "+media.toFixed(2);
 
-			   
-			
 
-}());
+					}
+					else if(media >=5  && media <7){
+						  $('.bd-show-modal-sm ,modal-content').modal();
+						msg.innerText=" O aluno foi  Aprovado com a média "+media.toFixed(2);
+
+						msg.innerText=(" O aluno ficou em  Recuperação com a média "+media.toFixed(2));
+
+					}else{
+						 $('.bd-show-modal-sm ,modal-content').modal();
+						msg.innerText=" O aluno foi  Aprovado com a média "+media.toFixed(2);
+
+						msg.innerText=(" O aluno ficou em  Recuperação com a média "+media.toFixed(2));
+
+					}
+				}
+							   
+				
+
+	}());
