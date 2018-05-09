7a608893dde14fbd4746240e0b1beb87b5f582be
Juniormartins26 <38302838+Juniormartins26@users.noreply.github.com>
2018-05-08 21:17:06 -0300

    simplificando a função que  calcula a média

--- script.js
+++ script.js
@@ -4,17 +4,17 @@
 			  nota1 = document.querySelector('#nota01');
 			  nota2 = document.querySelector('#nota02');
 			  nota3 = document.querySelector('#nota03');
+			  nota3 = document.querySelector('#nota03');
 			  nota4 = document.querySelector('#nota04');
 			  let msg = document.querySelector('#msg-modal');
 			  let   msgObrigatorio = document.querySelector('#msg');
+
+  			  let modal = $('.bd-show-modal-sm ,modal-content');
+
 			  let btnLimpar = document.querySelector('#btnLimpar');
 			 let botao =  document.querySelector('#btnCalcular');
 
 			 botao.addEventListener('click',mediaCal,false);
-			 	// $('#btnCalcular').click (function(){
-			 	// 	$('#modal-mensagem').modal();
-
-			 	// });
 
 				limp();
 				
@@ -28,6 +28,7 @@
 					nota1 = parseFloat(nota1.value);
 					nota2 = parseFloat(nota2.value);
 					nota3 = parseFloat(nota3.value);
+					nota4 = parseFloat(nota4.value);
 
 					media = (nota1 + nota2 + nota3 + nota4 )/4;
 					
@@ -41,29 +42,30 @@
 					btnLimpar.addEventListener('click', function(){
 				
 						 window.location.reload();
+						
 			 	},false);
 
 				 
 						
 				}
 				 
-				
+		
 				function valicao(){
 					if(media >= 7){
 					
-						 $('.bd-show-modal-sm ,modal-content').modal();
+						 modal.modal();
 						
 						msg.innerText=" Aluno foi Aprovado com a média "+media.toFixed(2);
 
 
 					}
 					else if(media >=5  && media <7){
-						  $('.bd-show-modal-sm ,modal-content').modal();
+						 modal.modal();
 						
 						msg.innerText=(" O aluno ficou em  Recuperação com a média "+media.toFixed(2));
 
 					}else{
-						 $('.bd-show-modal-sm ,modal-content').modal();
+						 modal.modal();
 						
 						msg.innerText=(" O aluno foi reprovado com a média "+media.toFixed(2));
 
